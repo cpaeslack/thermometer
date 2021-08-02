6958674042a842519c433a2ef39a4ffefc3a33b5
cpaeslack <christopher.paeslack@gmail.com>
2021-08-02 13:13:51 +0200

    Add influxdb connection, write datapoints with timestamp into db

--- thermometer.py
+++ thermometer.py
@@ -1,9 +1,35 @@
 import glob
 import time
+import datetime
+import argparse
+from influxdb import InfluxDBClient
 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+def get_args():
+    '''This function parses and returns arguments passed in'''
+    # Assign description to the help doc
+    parser = argparse.ArgumentParser(description='Program writes sensor measurement data to specified influx db.')
+    # Add arguments
+    parser.add_argument(
+    '-db','--database', type=str, help='Database name', required=True)
+    parser.add_argument(
+    '-sn','--session', type=str, help='Session', required=True)
+    now = datetime.datetime.now()
+    parser.add_argument(
+    '-rn','--run', type=str, help='Run number', required=False, default=now.strftime("%Y%m%d%H%M"))
+    parser.add_argument(
+    '-dt','--samplingrate', type=int, help='Sampling rate in seconds (default: 60)', required=False, default=60)
+    # Array of all arguments passed to script
+    args=parser.parse_args()
+    # Assign args to variables
+    dbname=args.database
+    runNo=args.run
+    session=args.session
+    sampling_rate=args.samplingrate
+    return dbname, session, runNo, sampling_rate
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -22,6 +48,44 @@ def read_temp():
         temp = float(temp_string) / 1000.0
         return temp
 
-while True:
-    print(read_temp())
-    time.sleep(30)
+def get_data_points():
+    # Get the three measurement values from the SenseHat sensors
+    current_temp = read_temp()
+    timestamp = datetime.datetime.utcnow().isoformat()
+
+    # Create Influxdb datapoints (using lineprotocol as of Influxdb >1.1)
+    datapoints = [
+        {
+            "measurement": session,
+            "tags": {"runNum": runNo},
+            "time": timestamp,
+            "fields": {
+                "temp_now": cpu_usage,
+            }
+        }
+    ]
+    return datapoints
+
+# Match return values from get_arguments()
+# and assign to their respective variables
+dbname, session, runNo, sampling_rate = get_args()
+print "Session: ", session
+print "Run No: ", runNo
+print "DB name: ", dbname
+print "Sampling rate: ", sampling_rate
+
+# Initialize the Influxdb client
+client = InfluxDBClient(host, port, user, password, dbname)
+try:
+    while True:
+        # Write datapoints to InfluxDB
+        datapoints=get_data_points()
+        bResult=client.write_points(datapoints)
+        print("Write points {0} Bresult:{1}".format(datapoints,bResult))
+
+        # Wait for next sample
+        time.sleep(sampling_rate)
+
+# Run until keyboard ctrl-c
+except KeyboardInterrupt:
+    print ("Program stopped by keyboard interrupt [CTRL_C] by user. ")
