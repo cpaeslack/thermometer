16baceec5c90117ba2a315162b468d2defabc2e9
cpaeslack <christopher.paeslack@gmail.com>
2021-08-07 16:54:26 +0200

    do some error catching and improvement to run script

--- run.sh
+++ run.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 
+helptext="provide type of sensor ('DHT22' or 'DS18B20')"
+
 if [ -z "$1" ]
 then
-    echo "provide type of sensor ('DHT22' or 'DS18B20 ')"
+    echo ${helptext}
     exit
 fi
 
-fname=thermometer_${1}.py
+sensor=${1}
+fname=thermometer_${sensor}.py
 
-python ${fname} -db=sensordata -sn=test
+if [ "${sensor}" == "DHT22" ]
+then
+    python ${fname} -db=sensordata -sn=test -gpio=D4 -v yes -dt 10
+elif [ "${sensor}" == "DS18B20" ]
+then
+    python ${fname} -db=sensordata -sn=test -v yes -dt 10
+else
+    echo ${helptext}
+    exit
+fi
