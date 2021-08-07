d864c7daa22155fd3642a13074ae583108dbc036
cpaeslack <christopher.paeslack@gmail.com>
2021-08-07 05:28:03 +0200

    add input arg to run.sh, rename python file

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
 
-python thermometer.py -db=sensordata -sn=test
+if -z ${1}
+then
+    echo "provide type of sensor ('DHT22' or 'DS18B20 ')"
+
+fname=thermometer_${1}.py
+
+python ${fname} -db=sensordata -sn=test
