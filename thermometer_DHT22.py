de575ca3f3e56ba4ff286d390ca863faca13e076
cpaeslack <christopher.paeslack@gmail.com>
2023-06-03 16:48:57 +0200

    refactor dht22 code

--- thermometer_DHT22.py
+++ thermometer_DHT22.py
@@ -1,111 +1,171 @@
-import glob
-import time
-import datetime
 import argparse
-import board
-import adafruit_dht
+import datetime
+from dotenv import load_dotenv
+import logging
+import os
+import time
+import Adafruit_DHT
 from influxdb import InfluxDBClient
 
-# Set required InfluxDB parameters.
-host = "192.168.178.51" #Could also set local ip address
-port = 8086
-user = "dbuser"
-password = "07Gn-xaYj-CGbc-y1s1"
+
+class Sensor:
+    """Class representing the DHT22 temperature/humidity sensor"""
+
+    def __init__(self, sensor_pin) -> None:
+        self.SENSOR = Adafruit_DHT.DHT22
+        self.SENSOR_PIN = sensor_pin
+
+    def read_sensor(self):
+        humidity, temperature = Adafruit_DHT.read_retry(
+            self.SENSOR, self.SENSOR_PIN
+        )
+        return temperature, humidity
+
+    def prepare_datapoints(self, session, runNo):
+        # Get the three measurement values from the SenseHat sensors
+        current_temp, current_humidity = self.read_sensor()
+        logging.info(f"temp: {current_temp}, hum: {current_humidity}")
+        timestamp = datetime.datetime.utcnow().isoformat()
+
+        # Create Influxdb datapoints (using lineprotocol as of Influxdb >1.1)
+        datapoints = [
+            {
+                "measurement": session,
+                "tags": {"runNum": runNo},
+                "time": timestamp,
+                "fields": {
+                    "temp_now_dht22": current_temp,
+                    "humid_now_dht22": current_humidity,
+                },
+            }
+        ]
+        return datapoints
+
+
+class Database:
+    """Class representing the InfluxDB database connection"""
+
+    def __init__(self, host, port, user, password, dbname) -> None:
+        self.client = InfluxDBClient(host, port, user, password, dbname)
+
+    def store_data(self, data, be_verbose):
+        try:
+            bResult = self.client.write_points(data)
+            if be_verbose == "yes":
+                logging.info(
+                    "Write points {0} Bresult:{1}".format(data, bResult)
+                )
+        except Exception as e:
+            logging.error(f"Failed to write to database. Reason: {e}")
+            raise e
+
+    def close_connection(self):
+        self.client.close()
+
+
+def main():
+    """Main loop of the program, calls the subprocesses after 10 seconds if they are not running"""
+
+    # Set format of log messages
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(message)s",
+        datefmt="[%Y-%m-%d %H:%M:%S]",
+        level=logging.INFO,
+    )
+
+    load_dotenv()
+    HOST = os.getenv("HOST")
+    PORT = os.getenv("PORT")
+    USER = os.getenv("USER")
+    PASSWORD = os.getenv("PASSWORD")
+    SENSOR_PIN = os.getenv("SENSOR_PIN")
+
+    # Match return values from get_arguments()
+    # and assign to their respective variables
+    dbname, session, runNo, sampling_rate, be_verbose = get_args()
+    logging.info("Session: ", session)
+    logging.info("Run No: ", runNo)
+    logging.info("DB name: ", dbname)
+    logging.info("Sampling rate: ", sampling_rate)
+    logging.info("Verbose mode: ", be_verbose)
+
+    while True:
+        sensor = Sensor(SENSOR_PIN)
+        try:
+            # Create database connection
+            database = Database(HOST, PORT, USER, PASSWORD, dbname)
+            # Get datapoints and write to database
+            datapoints = sensor.prepare_datapoints(session, runNo)
+            database.store_data(datapoints, be_verbose)
+            # Close database connection
+            database.close_connection()
+            # Wait for next sample
+            time.sleep(sampling_rate)
+
+        except RuntimeError as e:
+            # GPIO access may require sudo permissions
+            # Other RuntimeError exceptions may occur, but
+            # are common.  Just try again.
+            logging.error(f"RuntimeError: {e}")
+            logging.error("GPIO Access may need sudo permissions.")
+
+            time.sleep(2.0)
+            continue
+
+        except KeyboardInterrupt:
+            logging.info(
+                "Program stopped by keyboard interrupt [CTRL+C] by user. "
+            )
+
 
 def get_args():
-    '''This function parses and returns arguments passed in'''
+    """This function parses and returns arguments passed in"""
     # Assign description to the help doc
-    parser = argparse.ArgumentParser(description='Program writes sensor measurement data to specified influx db.')
+    parser = argparse.ArgumentParser(
+        description="Program writes sensor measurement data to specified influx db."
+    )
     # Add arguments
     parser.add_argument(
-    '-db','--database', type=str, help='Database name', required=True)
+        "-db", "--database", type=str, help="Database name", required=True
+    )
     parser.add_argument(
-    '-sn','--session', type=str, help='Session', required=True)
+        "-sn", "--session", type=str, help="Session", required=True
+    )
     now = datetime.datetime.now()
     parser.add_argument(
-    '-rn','--run', type=str, help='Run number', required=False, default=now.strftime("%Y%m%d%H%M"))
+        "-rn",
+        "--run",
+        type=str,
+        help="Run number",
+        required=False,
+        default=now.strftime("%Y%m%d%H%M"),
+    )
     parser.add_argument(
-    '-dt','--samplingrate', type=int, help='Sampling rate in seconds (default: 60)', required=False, default=60)
+        "-dt",
+        "--samplingrate",
+        type=int,
+        help="Sampling rate in seconds (default: 60)",
+        required=False,
+        default=60,
+    )
     parser.add_argument(
-    '-v','--verbose', type=str, help='Be loud and noisy', required=False, default="no")
+        "-v",
+        "--verbose",
+        type=str,
+        help="Be loud and noisy",
+        required=False,
+        default="no",
+    )
     # Array of all arguments passed to script
-    args=parser.parse_args()
+    args = parser.parse_args()
     # Assign args to variables
-    dbname=args.database
-    runNo=args.run
-    session=args.session
-    sampling_rate=args.samplingrate
+    dbname = args.database
+    runNo = args.run
+    session = args.session
+    sampling_rate = args.samplingrate
     be_verbose = args.verbose
     return dbname, session, runNo, sampling_rate, be_verbose
 
-def initialize_dht():
-    # Initialize the dht device, with data pin connected to:
-    # dhtDevice = adafruit_dht.DHT22(board.D4)
 
-    # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-    # This may be necessary on a Linux single board computer like the Raspberry Pi,
-    # but it will not work in CircuitPython.
-    dhtDevice = adafruit_dht.DHT22(board.D6, use_pulseio=False)
-    return dhtDevice
-
-def read_sensor():
-    temperature = dhtDevice.temperature
-    humidity = dhtDevice.humidity
-    return temperature, humidity
-
-def get_data_points():
-    # Get the three measurement values from the SenseHat sensors
-    current_temp, current_humidity = read_sensor()
-    timestamp = datetime.datetime.utcnow().isoformat()
-
-    # Create Influxdb datapoints (using lineprotocol as of Influxdb >1.1)
-    datapoints = [
-        {
-            "measurement": session,
-            "tags": {"runNum": runNo},
-            "time": timestamp,
-            "fields": {
-                "temp_now_dht22": current_temp,
-                "humid_now_dht22" : current_humidity,
-            }
-        }
-    ]
-    return datapoints
-
-# Match return values from get_arguments()
-# and assign to their respective variables
-dbname, session, runNo, sampling_rate, be_verbose = get_args()
-print("Session: ", session)
-print("Run No: ", runNo)
-print("DB name: ", dbname)
-print("Sampling rate: ", sampling_rate)
-print("Verbose mode: ", be_verbose)
-
-# Initialize dht sensor
-dhtDevice = initialize_dht()
-
-# Initialize the Influxdb client
-client = InfluxDBClient(host, port, user, password, dbname)
-
-while True:
-    try:
-        # Write datapoints to InfluxDB
-        datapoints=get_data_points()
-        bResult=client.write_points(datapoints)
-        if be_verbose == "yes":
-            print("Write points {0} Bresult:{1}".format(datapoints,bResult))
-
-        # Wait for next sample
-        time.sleep(sampling_rate)
-
-    except RuntimeError as error:
-        print(error.args[0])
-        time.sleep(sampling_rate)
-        continue
-
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    except KeyboardInterrupt:
-        print("Program stopped by keyboard interrupt [CTRL+C] by user. ")
+if __name__ == "__main__":
+    main()
